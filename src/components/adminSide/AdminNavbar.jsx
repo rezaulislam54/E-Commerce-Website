8fb75f9c22e50300b74293af2fd20bef02472ac4
rezaulislam54 <rezaulislam5490@gmail.com>
2025-02-07 17:18:21 +0600

    dashboard added

---
+++ src/components/adminSide/AdminNavbar.jsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+
+const AdminNavbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link to={"/"} className="btn btn-ghost text-xl">
+          SHOP.CO
+        </Link>
+      </div>
+      <div className="flex-none gap-2 mr-5">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+        </div>
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-40 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminNavbar;
---
+++ src/layouts/DashboardLayoutes.jsx
@@ -0,0 +1,116 @@
+import { useContext, useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+import AdminNavbar from "../components/adminSide/AdminNavbar";
+import { FaRegUserCircle } from "react-icons/fa";
+import { MdManageHistory } from "react-icons/md";
+import { RxCross1 } from "react-icons/rx";
+import { RiSecurePaymentLine } from "react-icons/ri";
+import { LiaUserEditSolid } from "react-icons/lia";
+import { TbLogout } from "react-icons/tb";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { AuthContext } from "../authProvider/AuthContextProvider";
+
+const DashboardLayoutes = () => {
+  const [show, setShow] = useState(false);
+  const { LognOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    LognOut().then(() => {
+      navigate("/");
+    });
+  };
+  return (
+    <div>
+      <div className="min-h-screen flex">
+        <div
+          className={`bg-theme_primary text-white ${show ? "w-1/4" : "w-16"}`}
+          id="sidebar"
+        >
+          <div className="p-5 flex justify-between text-2xl cursor-pointer">
+            <div className="">
+              {show || <GiHamburgerMenu onClick={() => setShow(true)} />}
+            </div>
+            <div className="font-bold">
+              {show && <RxCross1 onClick={() => setShow(false)} />}
+            </div>
+          </div>
+
+          <div className="pt-10">
+            {/* user profile */}
+            <Link
+              to={`/dashboard`}
+              className="flex gap-3 items-center px-5 py-3 text-2xl hover:bg-white hover:text-black cursor-pointer"
+            >
+              <div
+                className="tooltip  tooltip-right"
+                data-tip={`${show ? "" : "User Profile"}`}
+              >
+                <FaRegUserCircle />
+              </div>
+              <p className={`${show ? "" : "hidden"}`}>User Profile</p>
+            </Link>
+            {/* update Profile */}
+            <Link
+              to={`/dashboard/update-user`}
+              className="flex gap-3 items-center px-5 py-3 text-2xl hover:bg-white hover:text-black cursor-pointer"
+            >
+              <div
+                className="tooltip  tooltip-right"
+                data-tip={`${show ? "" : "update Profile"}`}
+              >
+                <LiaUserEditSolid />
+              </div>
+              <p className={`${show ? "" : "hidden"}`}>update Profile</p>
+            </Link>
+            {/* manage product */}
+            <div className="flex gap-3 items-center px-5 py-3 text-2xl hover:bg-white hover:text-black cursor-pointer">
+              <div
+                className="tooltip  tooltip-right"
+                data-tip={`${show ? "" : "Manage Product"}`}
+              >
+                <MdManageHistory />
+              </div>
+              <p className={`${show ? "" : "hidden"}`}>Manage Product</p>
+            </div>
+            {/* payment */}
+            <div className="flex gap-3 items-center px-5 py-3 text-2xl hover:bg-white hover:text-black cursor-pointer">
+              <div
+                className="tooltip  tooltip-right"
+                data-tip={`${show ? "" : "Payment"}`}
+              >
+                <RiSecurePaymentLine />
+              </div>
+              <p className={`${show ? "" : "hidden"}`}>Payment</p>
+            </div>
+            {/* logout */}
+            <div
+              onClick={handleLogout}
+              className="flex gap-3 items-center px-5 py-3 text-2xl hover:bg-white hover:text-black cursor-pointer"
+            >
+              <div
+                className="tooltip  tooltip-right"
+                data-tip={`${show ? "" : "Logout"}`}
+              >
+                <TbLogout />
+              </div>
+              <p className={`${show ? "" : "hidden"}`}>Logout</p>
+            </div>
+          </div>
+        </div>
+        <div className=" flex-1" id="mainArea">
+          <div className=" shadow-md">
+            <AdminNavbar />
+          </div>
+
+          {/* outlet */}
+          <div>
+            <Outlet></Outlet>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayoutes;
